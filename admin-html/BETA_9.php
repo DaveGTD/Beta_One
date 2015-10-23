465b107402b0ff1dcd41bdc130ab5d9980ad2c7a
Dave Aditya <adave@adobe.com>
2015-10-22 23:14:29 -0600

    none

--- admin-html/BETA_9.php
+++ admin-html/BETA_9.php
@@ -1,46 +1,47 @@
-<html>
-	<head>
-	</head>
-	
-	
-	<body>
-		
-		<?php
-			
-			function maketable($query, $fieldarray)
-			{
-				//count number of columns
-				 $columns = count($fieldarray);
-				 
-				 $conn = mysqli_connect('localhost', 'root', 'root', 'beta');
-				 if(!$conn) 
-				 {
-				 	die("Connection failed: " . mysqli_connect_error());
-				 }
-				 
-				 $result = mysqli_query($conn, $query);
-				 $itemnum = mysqli_num_rows($result);
-				 if($itemnum > 0)
-				 { 
-						do
-					 	{   
-							 echo "< tr >" ;
-							 for($x = 0; $x < $columns; $x++)
-							 {
-							 	echo "< td >" .$items[$fieldarray[$x]]. "< /td >" ;
-			   				 }
-			   				 echo "< /tr >" ;
-			  			}
-			  			while($items = mysqli_fetch_assoc($result));
-			 	 }
-			 }
-			
+<html><head><title>MySQL Table Viewer</title></head><body>
+<?php
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pwd = 'root';
 
-			$fieldarray = array('row_num', 'id', 'name', 'title', 'salary', 'start_date', 'x', 'y');
-			maketable("SELECT * FROM employees", $fieldarray);
-	
-		?>
-		
-	</body>
-	
-</html>
+$database = 'beta';
+$table = 'employees';
+
+if (!mysql_connect($db_host, $db_user, $db_pwd))
+    die("Can't connect to database");
+
+if (!mysql_select_db($database))
+    die("Can't select database");
+
+// sending query
+$result = mysql_query("SELECT * FROM {$table}");
+if (!$result) {
+    die("Query to show fields from table failed");
+}
+
+$fields_num = mysql_num_fields($result);
+
+echo "<h1>Table: {$table}</h1>";
+echo "<table border='1'><tr>";
+// printing table headers
+for($i=0; $i<$fields_num; $i++)
+{
+    $field = mysql_fetch_field($result);
+    echo "<td>{$field->name}</td>";
+}
+echo "</tr>\n";
+// printing table rows
+while($row = mysql_fetch_row($result))
+{
+    echo "<tr>";
+
+    // $row is array... foreach( .. ) puts every element
+    // of $row to $cell variable
+    foreach($row as $cell)
+        echo "<td>$cell</td>";
+
+    echo "</tr>\n";
+}
+mysql_free_result($result);
+?>
+</body></html>
